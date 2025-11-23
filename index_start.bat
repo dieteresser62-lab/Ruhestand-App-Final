7b9bfa205f5bdf8eb120209dcd667ea9bf31359e
Dieter Esser <dieteresser62@gmail.com>
2025-11-23 11:15:36 +0100

    Redesign index page and update start script

--- index_start.bat
+++ index_start.bat
@@ -1,14 +1,16 @@
 @echo off
-echo Starte den Ruhestands-Simulator...
+echo Starte Server... und wecke den RAM-Fresser Chrome...
 echo (Fenster offen lassen, sonst ist der Server weg!)
 
-:: Wechselt in das Verzeichnis, in dem dieses Skript liegt
+:: 1. Verzeichnis wechseln
 cd /d "%~dp0"
 
-:: Öffnet den Browser automatisch mit der funktionierenden IP
-start http://127.0.0.1:8000/index.html
+:: 2. Chrome explizit starten
+:: Der erste leere String "" ist wichtig, sonst denkt Windows, der Pfad ist der Fenstertitel.
+:: Pfad ist Standard-Windows. Falls dein Chrome woanders liegt, musst du den Pfad anpassen.
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" http://127.0.0.1:8000/index.html
 
-:: Startet den Server stur auf IPv4
+:: 3. Server starten (stur auf IPv4)
 python -m http.server 8000 --bind 127.0.0.1
 
 pause
